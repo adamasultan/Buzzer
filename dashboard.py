27fcf657a0851f989ed5a99467c7ed9953cd34a0
adamasultan <112110114+adamasultan@users.noreply.github.com>
2023-09-01 17:37:56 -0400

    bug fix
    bug where all lights wouldnt update

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,17 @@ class Dashboard:
 
   def add_leds(self, arr):
     self.__leds += arr
-
+  def blank(self):
+    self.__pixels.fill((0,0,0))
+    self.__pixels.write()
+    sleep(0.05)
+    self.__pixels.write()
+    
+  def set_white(self):
+    self.__pixels.fill((75,75,75))
+    self.__pixels.write()
+    sleep(0.05)
+    self.__pixels.write()
   def win(self):
     print("win")
     rainbow = [
@@ -20,10 +30,14 @@ class Dashboard:
     (30 , 97 , 0),(18 , 109 , 0),(6 , 121 , 0),(0 , 122 , 5),(0 , 110 , 17),(0 , 98 , 29),(0 , 86 , 41),(0 , 74 , 53),
     (0 , 62 , 65),(0 , 50 , 77),(0 , 38 , 89),(0 , 26 , 101),(0 , 14 , 113),(0 , 2 , 125),(9 , 0 , 118),(21 , 0 , 106),
     (33 , 0 , 94),(45 , 0 , 82),(57 , 0 , 70),(69 , 0 , 58),(81 , 0 , 46),(93 , 0 , 34),(105 , 0 , 22),(117 , 0 , 10)]
-
-    self.__pixels.fill((0,0,0)) #fills whole led strip which it shouldn't have full control
+    
+    
+    self.blank()
     for index in self.__leds:
-      self.__pixels[index] = self.__color
+        self.__pixels[index] = self.__color
+    #self.__pixels.fill(self.__color)
+    self.__pixels.write()
+    sleep(0.05)
     self.__pixels.write()
     sleep(2)
     count = 30
@@ -36,12 +50,11 @@ class Dashboard:
       count = count - 1
 
   def reset(self):
-    reset_color = (75,75,75)
-    for index in self.__leds:
-      self.__pixels[index] = reset_color
-    self.__pixels.write()
+    self.set_white()
+ 
 
   
+
 
   def neotest(self):
     rainbow = [
